330254d2c7c6033c58810ae37e8ae5897c314cc1
Ambre <ambre_donnadieu@live.fr>
2020-02-18 15:20:10 +0100

    Add json example

--- TP2_Code/TP2_Code/Program.cs
+++ TP2_Code/TP2_Code/Program.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TP2_Code
 {
     class Program
     {
+        //note : le fichier json utilisé pour les tests du projet a été pris du site suivant :
+        // https://support.oneskyapp.com/hc/en-us/articles/208047697-JSON-sample-files
+
+        Dictionary<string, Type> DicoFinal;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -15,5 +21,7 @@ namespace TP2_Code
         // 2- récupère les membres privés 
         // 3- récupère les sous-objets sauf tableaux et IEnumarable
         // 4- récupère les tableaux et IEnumerable
+
+
     }
 }
