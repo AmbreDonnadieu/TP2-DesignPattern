f8b6c4857c56180607907633a3c5db03c201c3ac
Ambre <ambre_donnadieu@live.fr>
2020-02-18 15:13:06 +0100

    Comment code

--- TP2_Code/TP2_Code/Program.cs
+++ TP2_Code/TP2_Code/Program.cs
@@ -8,5 +8,12 @@ namespace TP2_Code
         {
             Console.WriteLine("Hello World!");
         }
+
+
+        //JSONSerialize(jsonfile)
+        // 1- récupère tous les json objects simple (int, char etc + string)
+        // 2- récupère les membres privés 
+        // 3- récupère les sous-objets sauf tableaux et IEnumarable
+        // 4- récupère les tableaux et IEnumerable
     }
 }
