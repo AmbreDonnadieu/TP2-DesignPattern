cadf690c83aae43fe176535f67f21c639d7e6eaf
Ambre <ambre_donnadieu@live.fr>
2020-02-21 16:02:03 +0100

    creation objet test

--- TP2_Code/TP2_Code/Program.cs
+++ TP2_Code/TP2_Code/Program.cs
@@ -15,19 +15,11 @@ namespace TP2_Code
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            bool existance = File.Exists("Jsontest.json");
-            
-            Console.WriteLine(existance);
 
+            List<DateTime> datesTest = new List<DateTime>();
+            datesTest.Add(new DateTime(2019, 10, 31));
 
-            //lire le json ici
-            string typeJson = new string("pomme");
-            char[] chars = new char[] { 'm', 'a', 'm', 'a' };
-            object json = Activator.CreateInstance(typeJson.GetType(), new Object[] { chars} ) ;
-            
-            
-            Console.WriteLine(json.GetType().ToString() +" : " + json.ToString());
-
+            Chat miaou = new Chat("Marie", 1, "Angorra", Chat.CouleursYeux.Bleu, Chat.TypePoil.Long, datesTest);
 
             object Poire = new object();
             Poire = "poire";
@@ -58,19 +50,19 @@ namespace TP2_Code
             public string Race { get { return race; } }  
             public CouleursYeux Yeux { get { return yeuxchat; } }  
             public TypePoil Poil { get { return poilchat; } }    
-            List<DateTime> Vaccin { get { return DatesVaccination; } }  
+            List<DateTime> Vaccin { get { return DatesVaccination; } }
 
+            public Chat(string nom, int age, string race, CouleursYeux yeux, TypePoil poil, List<DateTime> Vaccination)
+            {
+                this.nom = nom;
+                this.race = race;
+                yeuxchat = yeux;
+                poilchat = poil;
+                DatesVaccination = Vaccination;
+            }
 
-}
+        }
 
-        //JSONSerialize(jsonfile)
-        // 1- récupère tous les json objects simple (int, char etc + string)
-        // 2- récupère les membres privés 
-        // 3- récupère les sous-objets sauf tableaux et IEnumarable
-        // 4- récupère les tableaux et IEnumerable
-
-        // a- parcourir le fichier json comme un arbre
-        // b- à chaque noeud, tu checks les conditions ci-dessus pour voir si tu l'ajoutes au dico ou non
-        //en gros, on veut convertir un fichier json en dictionary <string, object>
+        
     }
 }
