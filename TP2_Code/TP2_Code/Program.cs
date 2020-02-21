77c3fa06a22a1e0fca2e3ad50b9615ec9882ca29
Ambre <ambre_donnadieu@live.fr>
2020-02-21 16:52:13 +0100

    lecture et ecriture dans myJSon

--- TP2_Code/TP2_Code/Program.cs
+++ TP2_Code/TP2_Code/Program.cs
@@ -12,18 +12,21 @@ namespace TP2_Code
 
         static void Main(string[] args)
         {
-            MyJson DicoFinal = new MyJson();
-            //DicoFinal.
+            Dictionary<string, object> DicoFinal = new MyJson();
 
             Console.WriteLine("Hello World!");
 
             List<DateTime> datesTest = new List<DateTime>();
             datesTest.Add(new DateTime(2019, 10, 31));
+            
+            Animal Bleriot = new Chat("Marie","Angorra", Chat.CouleursYeux.Bleu, Chat.TypePoil.Long, datesTest);
+            Console.WriteLine(Bleriot.age);
+            Bleriot.age = 1;
 
-            Chat miaou = new Chat("Marie", 1, "Angorra", Chat.CouleursYeux.Bleu, Chat.TypePoil.Long, datesTest);
 
-            //DicoFinal.JsonSerialize();
+            DicoFinal.Add("chat1",Bleriot);
 
+            Console.WriteLine(DicoFinal["chat1"].ToString() );
 
             Console.ReadKey();
 
@@ -43,26 +46,32 @@ namespace TP2_Code
             
         }
 
-        class Chat
+
+        public class Animal
+        {
+            public int age;
+            public Animal(int age) { this.age = age; }
+        }
+        class Chat : Animal
         {
             public enum CouleursYeux { Bleu, Jaune, marron, vert };
             public enum TypePoil { Long, court, ras };
 
             public string nom;
-            public int age;
+            //public int age;
             public string race;
             public CouleursYeux yeuxchat;
             public TypePoil poilchat;
             List<DateTime> DatesVaccination;
 
             public string Nom { get { return nom; } }
-            public int Age { get { return age; } }
+           // public int Age { get { return age; } }
             public string Race { get { return race; } }
             public CouleursYeux Yeux { get { return yeuxchat; } }
             public TypePoil Poil { get { return poilchat; } }
             List<DateTime> Vaccin { get { return DatesVaccination; } }
 
-            public Chat(string nom, int age, string race, CouleursYeux yeux, TypePoil poil, List<DateTime> Vaccination)
+            public Chat(string nom,  string race, CouleursYeux yeux, TypePoil poil, List<DateTime> Vaccination) : base(0)
             {
                 this.nom = nom;
                 this.race = race;
@@ -71,6 +80,11 @@ namespace TP2_Code
                 DatesVaccination = Vaccination;
             }
 
+            public override string ToString()
+            {
+                string varia = "Le chat s'appelle " + Nom + " et il a " + age + " années.";
+                return varia;
+            }
         }
 
 
